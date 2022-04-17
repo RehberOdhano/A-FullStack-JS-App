f296e1e8584cb4a23692e54717dbb684f0c28f00
RehberOdhano <rehber.odhano30@gmail.com>
2022-04-17 17:07:05 +0500

    added styles to the homepage and set the view-engine

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 
+// now express knows where to find the our html templates
+app.set('views', 'views');
+// telling express to use ejs as a view-engine
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.send("Welcome to our new app!");
+    res.render('home-guest');
 });
 
 
