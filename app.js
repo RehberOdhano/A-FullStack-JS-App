43f6d8cea5e151a9b752d1162bc4da992b5546d5
RehberOdhano <rehber.odhano30@gmail.com>
2022-04-17 16:31:46 +0500

    starting to implement a new app...!

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Welcome to our new app!");
+});
+
+
+app.listen(3000, () => {
+    console.log("Server is started...");
+});
