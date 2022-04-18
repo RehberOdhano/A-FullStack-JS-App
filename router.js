f8369f448bd52eb4bbb9bfcc3620c110342291f2
RehberOdhano <rehber.odhano30@gmail.com>
2022-04-18 22:20:58 +0500

    implementing login functionality...

--- router.js
+++ router.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 router.get('/', userController.home);
 router.post('/register', userController.register);
+router.post('/login', userController.login);
 
 
 module.exports = router;
